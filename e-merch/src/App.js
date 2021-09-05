e5f8a2a9560700e36027d22dbc529d9fff3342d3
zethcode <89132475+zethcode@users.noreply.github.com>
2021-09-05 17:46:16 +0800

    Minor material ui changes for the UI wireframe

--- e-merch/src/App.js
+++ e-merch/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/ToolBar';
+import { makeStyles, Container, Grid, ThemeProvider, createTheme } from '@material-ui/core';
+import Footer from './components/Footer';
 
 function App() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1
+    }
+  }));
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: [
+        "Montserrat", 
+        "sans-serif"
+      ].join(',')
+    }
+  })
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Zeth Garments
+            </Typography>
+            <Button color="inherit">
+              Login
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <Container fixed maxWidth="lg" style={{backgroundColor: "#82b9d1"}}>
+          <Grid item>
+            Test
+          </Grid>
+        </Container>
+      </ThemeProvider>
+      <Footer />
     </div>
   );
 }
---
+++ e-merch/src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>All rights reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
